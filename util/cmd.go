8e117b4d81d5df0f37ba1565acfe73d398237740
ExcaliburZhou <ExcaliburZhou@github.com>
2016-09-08 17:20:06 +0800

    exec cmd without bash

--- util/cmd.go
+++ util/cmd.go
@@ -28,7 +28,7 @@ func Run(command , appName, appNamespace string) error {
 }
 
 func ExecCommand(command string) ([]byte, error) {
-	cmd := exec.Command("/bin/sh", "-c", command)
+	cmd := exec.Command(command)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		Logger.Error("StdoutPipe: " + err.Error())
